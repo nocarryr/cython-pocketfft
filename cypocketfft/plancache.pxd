39f6ebf8a97e28c0228469172037e1da957ee9f1
nocarryr <matt@nomadic-recording.com>
2019-06-26 13:16:22 -0500

    Correct error in plan insertion and use a mutex for container modification

--- cypocketfft/plancache.pxd
+++ cypocketfft/plancache.pxd
@@ -9,9 +9,21 @@ ctypedef cpp_pair[size_t, rfft_plan] RFFT_PLAN_ITEM_t
 ctypedef cpp_map[size_t, cfft_plan] CFFT_PLAN_MAP_t
 ctypedef cpp_pair[size_t, cfft_plan] CFFT_PLAN_ITEM_t
 
+cdef extern from "<mutex>" namespace "std" nogil:
+    cdef cppclass mutex:
+        mutex() except +
+        mutex(const mutex&) except +
+        mutex& operator=(const mutex&)# = delete
+
+        void lock()
+        bint try_lock()
+        void unlock()
+
+
 cdef class PlanCache:
     cdef RFFT_PLAN_MAP_t rfft_plans
     cdef CFFT_PLAN_MAP_t cfft_plans
+    cdef mutex _lock
 
     cdef rfft_plan get_rplan(self, size_t length, bint create=*) nogil except *
     cdef void del_rplan(self, size_t length) nogil except *
--- cypocketfft/plancache.pyx
+++ cypocketfft/plancache.pyx
@@ -30,17 +30,27 @@ cdef class PlanCache:
         if self.rfft_plans.count(length) == 0:
             if not create:
                 return NULL
-            plan = self._build_rplan(length)
+            self._lock.lock()
+            # Check to see if the plan was built while waiting for the lock
+            if self.rfft_plans.count(length) > 0:
+                plan = self.rfft_plans[length]
+            else:
+                plan = self._build_rplan(length)
+                self.rfft_plans.insert(RFFT_PLAN_ITEM_t(length, plan))
+            self._lock.unlock()
         else:
             plan = self.rfft_plans[length]
-            self.rfft_plans.insert(RFFT_PLAN_ITEM_t(length, plan))
         return plan
     cdef void del_rplan(self, size_t length) nogil except *:
         if self.rfft_plans.count(length) == 0:
             return
-        cdef rfft_plan plan = self.rfft_plans[length]
+        cdef rfft_plan plan
+
+        self._lock.lock()
+        plan = self.rfft_plans[length]
         self.rfft_plans.erase(length)
         wrapper._destroy_rfft_plan(plan)
+        self._lock.unlock()
     cdef rfft_plan _build_rplan(self, size_t length) nogil except *:
         cdef rfft_plan plan = wrapper._make_rfft_plan(length)
         if not plan:
@@ -50,12 +60,18 @@ cdef class PlanCache:
     cdef cfft_plan get_cplan(self, size_t length, bint create=True) nogil except *:
         cdef cfft_plan plan
         if self.cfft_plans.count(length) == 0:
-            plan = self._build_cplan(length)
             if not create:
                 return NULL
+            self._lock.lock()
+            # Check to see if the plan was built while waiting for the lock
+            if self.cfft_plans.count(length) > 0:
+                plan = self.cfft_plans[length]
+            else:
+                plan = self._build_cplan(length)
+                self.cfft_plans.insert(CFFT_PLAN_ITEM_t(length, plan))
+            self._lock.unlock()
         else:
             plan = self.cfft_plans[length]
-            self.cfft_plans.insert(CFFT_PLAN_ITEM_t(length, plan))
         return plan
     cdef cfft_plan _build_cplan(self, size_t length) nogil except *:
         cdef cfft_plan plan = wrapper._make_cfft_plan(length)
@@ -66,8 +82,12 @@ cdef class PlanCache:
     cdef void del_cplan(self, size_t length) nogil except *:
         if self.cfft_plans.count(length) == 0:
             return
-        cdef cfft_plan plan = self.cfft_plans[length]
+        cdef cfft_plan plan
+
+        self._lock.lock()
+        plan = self.cfft_plans[length]
         self.cfft_plans.erase(length)
         wrapper._destroy_cfft_plan(plan)
+        self._lock.unlock()
 
 cdef PlanCache plan_cache = PlanCache()
