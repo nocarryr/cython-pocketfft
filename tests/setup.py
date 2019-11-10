7831aed4396ec89b575b3d47c79871feeab57f6e
nocarryr <matt@nomadic-recording.com>
2019-11-10 10:54:07 -0600

    Disable openmp on macos and re-enable it in build

--- tests/setup.py
+++ tests/setup.py
@@ -1,9 +1,14 @@
+import sys
 from setuptools import setup, find_packages
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
 import cypocketfft
 
+if sys.platform == 'darwin':
+    COMPILE_ARGS = []
+else:
+    COMPILE_ARGS = ['-fopenmp']
 INCLUDE_PATH = [cypocketfft.get_include()]
 
 ext_modules = [
@@ -14,6 +19,8 @@ ext_modules = [
     Extension(
         '_testfft', ['_testfft.pyx'],
         include_dirs=INCLUDE_PATH,
+        extra_compile_args=COMPILE_ARGS,
+        extra_link_args=COMPILE_ARGS,
     ),
 ]
 
