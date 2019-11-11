29966e5f59aa450a4fae7550f4ac27cb0c115cf3
nocarryr <matt@nomadic-recording.com>
2019-11-11 10:45:29 -0600

    Use absolute paths for wheelhouse

--- ci/build_manywheels.sh
+++ ci/build_manywheels.sh
@@ -11,13 +11,13 @@ pys=(${pys[@]//*34*/})
 # Compile wheels
 for PYBIN in "${pys[@]}"; do
     "${PYBIN}/pip" install -r /io/requirements.txt
-    "${PYBIN}/pip" wheel /io/sdist/*.tar.gz -w wheelhouse/
+    "${PYBIN}/pip" wheel /io/sdist/*.tar.gz -w /io/wheelhouse/
 done
 
 ls /io/wheelhouse/
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/$package_name-*.whl; do
+for whl in /io/wheelhouse/$package_name-*.whl; do
     auditwheel repair --plat $PLAT "$whl" -w /io/wheelhouse/
 done
 
