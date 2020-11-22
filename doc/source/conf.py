4f3e97288b57eb8ee14cabe5278cb2915cbbeaa5
nocarryr <matt@nomadic-recording.com>
2020-11-22 09:45:24 -0600

    Add intersphinx doc sources for numpy

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -85,4 +85,7 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+    'python':('https://docs.python.org/', None),
+    'numpy':('https://numpy.org/doc/stable/', None),
+}
